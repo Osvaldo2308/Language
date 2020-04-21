122b66e70b8596ad30d6ba5d568cbbf469fadac0
Osvaldo2308 <62044722+Osvaldo2308@users.noreply.github.com>
2020-04-21 16:51:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/languaje/Util.java
@@ -0,0 +1,10 @@
+package com.example.languaje;
+
+import android.content.Context;
+
+public class Util {
+    public static String getWelcome(Context context){
+        return context.getString(R.string.welcome);
+
+    }
+}
